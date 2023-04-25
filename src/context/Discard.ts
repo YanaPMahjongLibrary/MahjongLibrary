f7597c8aefec8f08f182870c73d0e9da6054fc29
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 06:42:15 +0900

    捨て牌クラス定義

---
+++ src/context/Discard.ts
@@ -0,0 +1,17 @@
+import { Tile } from "../tile/Tile";
+
+/**
+ * 捨て牌クラス
+ */
+export class Discard {
+  private _tiles: Tile[];
+
+  constructor () {
+    this._tiles = [];
+  }
+
+  /**
+   * 捨て牌
+   */
+  get tiles(): Tile[] { return this._tiles.map(t => t.clone()); }
+}
