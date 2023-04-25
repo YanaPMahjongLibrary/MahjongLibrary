f167c88b71b096b276001af76728be25aea0a391
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 06:31:19 +0900

    鳴きを表すクラス

---
+++ src/context/Meld.ts
@@ -0,0 +1,24 @@
+import { Tile } from "../tile/Tile";
+
+/**
+ * 副露
+ */
+export class Meld {
+  /**
+   * コンストラクタ
+   * @param _tiles 牌のセット
+   * @param _from どこから鳴いたか
+   */
+  constructor (private _tiles: Tile[], private _from: number) {
+  }
+
+  /**
+   * 牌のセット
+   */
+  get tiles(): Tile[] { return this._tiles.map(t => t.clone() ); }
+
+  /**
+   * どこから鳴いたか
+   */
+  get from(): number { return this._from; }
+}
