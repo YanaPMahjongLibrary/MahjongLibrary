5cddf30fea848d31098242efb2ae1587af6cd589
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 06:39:36 +0900

    プレイヤーの手を表すクラスをとりあえず定義

---
+++ src/context/PlayerHand.ts
@@ -0,0 +1,26 @@
+import { Tile } from "../tile/Tile";
+import { Meld } from "./Meld";
+
+/**
+ * プレイヤーの手を表すクラス
+ * TODO: 手牌を制御する為のクラスを定義する
+ *       ※「他人が勝手に牌を切った」とかが無いように、このクラスでそういったメソッドは実装しない
+ *        　contextディレクトリ内にある定義は、全て「AIが参照するもの」と言う認識を持っておく事
+ */
+export class PlayerHand {
+  private _tiles: Tile[];
+  private _melds: Meld[];
+
+  /**
+   * コンストラクタ
+   */
+  constructor () {
+    this._tiles = [];
+    this._melds = [];
+  }
+
+  /**
+   * 鳴いた牌
+   */
+  get melds(): Meld[] { return this._melds; }
+}
