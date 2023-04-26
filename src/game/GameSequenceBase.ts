3a26dc8fdfdcb5a41184558688b1873b4c7114a2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-27 08:52:18 +0900

    一旦設計しなおし

---
+++ src/game/GameSequenceBase.ts
@@ -0,0 +1,31 @@
+import { IPlayer } from "../player/Player";
+
+/**
+ * ゲーム進行管理基底クラス
+ */
+export abstract class GameSequenceBase {
+  private players: IPlayer[];
+
+  /**
+   * コンストラクタ
+   */
+  constructor (private maximumPlayerCount: number = 4) {
+    this.players = [];
+  }
+
+  join (player: IPlayer) {
+    // 規定プレイヤー数以上の参加は許可しない
+    if (this.players.length + 1 > this.maximumPlayerCount) { return; }
+    
+    this.players.push(player);
+    if (this.players.length === this.maximumPlayerCount) {
+      // プレイヤーが揃ったら開始する
+      this.start();
+    }
+  }
+  
+  /**
+   * 開始
+   */
+  abstract start(): void;
+}
---
+++ src/player/Player.ts
@@ -0,0 +1,7 @@
+/**
+ * プレイヤーインタフェース
+ * AIクラス等はこれを実装する
+ */
+export interface IPlayer {
+
+}
