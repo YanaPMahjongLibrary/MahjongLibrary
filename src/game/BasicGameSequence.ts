b0a84b43012a3cebc7fdf1fb966ba4db31f45a63
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 04:19:57 +0900

    とりあえず普通の４人打ちの実装用クラス

---
+++ src/game/BasicGameSequence.ts
@@ -0,0 +1,20 @@
+import { GameSequenceBase } from "./GameSequenceBase";
+
+/**
+ * 普通の４人打ちルール
+ */
+export class BasicGameSequence extends GameSequenceBase {
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   * ゲーム開始
+   */
+  start(): void {
+    // TODO: 実装
+  }
+}
