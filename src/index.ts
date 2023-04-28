cb4aaadebfa3ea20934cce3607e5bab73e77ca77
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 04:41:44 +0900

    全てexportした

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-export { Tile } from "./tile/Tile";
-export { TileType, ETileKind } from "./tile/TileType";
+export * from "./context/BoardContext";
+export * from "./context/Discard";
+export * from "./context/Meld";
+export * from "./context/PlayerContext";
+export * from "./context/PlayerHand";
+export * from "./context/TileStack";
 
-export { RuleSet } from "./rule/RuleSet";
-export type { ICustomizableRule } from "./rule/CustomizableRule";
+export * from "./errors/ContextAccessHashError";
+
+export * from "./game/GameSequenceBase";
+export * from "./game/BasicGameSequence";
+
+export * from "./player/Player";
+
+export * from "./rule/RuleSet";
+export * from "./rule/CustomizableRule";
+
+export * from "./tile/Tile";
+export * from "./tile/TileType";
