4f7361bee3adf4c594707699e9e63e1b721030a7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 04:17:23 +0900

    インデックスは必要性を感じないので撤去

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -3,7 +3,6 @@
  * AIクラス等はこれを継承する
  */
 export abstract class PlayerBase {
-  private _index: number;
   private handAccessHash: string;
 
   /**
@@ -11,15 +10,7 @@ export abstract class PlayerBase {
    * @param index プレイヤーインデックス
    * @param handAccessHash 手牌アクセス用ハッシュ値
    */
-  constructor(index: number, handAccessHash: string) {
-    this._index = index;
+  constructor(handAccessHash: string) {
     this.handAccessHash = handAccessHash;
-  }
-
-  /**
-   * プレイヤーインデックス
-   */
-  get index(): number {
-    return this._index;
   }
 }
