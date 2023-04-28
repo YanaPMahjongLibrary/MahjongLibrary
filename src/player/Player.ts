7359d21ec9a19cf170043081449ea4064ca8a0ab
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 04:14:42 +0900

    プレイヤーはインタフェースではなく基底クラスとした

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -1,5 +1,25 @@
 /**
- * プレイヤーインタフェース
- * AIクラス等はこれを実装する
+ * プレイヤー基底クラス
+ * AIクラス等はこれを継承する
  */
-export interface IPlayer {}
+export abstract class PlayerBase {
+  private _index: number;
+  private handAccessHash: string;
+
+  /**
+   * コンストラクタ
+   * @param index プレイヤーインデックス
+   * @param handAccessHash 手牌アクセス用ハッシュ値
+   */
+  constructor(index: number, handAccessHash: string) {
+    this._index = index;
+    this.handAccessHash = handAccessHash;
+  }
+
+  /**
+   * プレイヤーインデックス
+   */
+  get index(): number {
+    return this._index;
+  }
+}
