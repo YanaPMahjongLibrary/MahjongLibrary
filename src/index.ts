e90bd2b558a9018765acaafbde801f51fece68cf
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-04 15:25:00 +0900

    一旦index.tsの中身を撤去

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Context } from "./context";
-export { Error } from "./errors";
-export { Game } from "./game";
-export { Player } from "./player";
-export { Rule } from "./rule";
-export { Tile } from "./tile";
