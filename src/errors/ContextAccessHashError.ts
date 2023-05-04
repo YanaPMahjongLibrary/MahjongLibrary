dfadef63965191c4828c0698902b401c29b893e4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-04 15:16:16 +0900

    一旦撤去

--- src/errors/ContextAccessHashError.ts
+++
@@ -1,8 +0,0 @@
-/**
- * コンテキストアクセスオブジェクト取得時にハッシュが食い違っていた時のエラー
- */
-export class ContextAccessHashError extends Error {
-  constructor() {
-    super("Not allowed get ContextAccess object.");
-  }
-}
--- src/rule/CustomizableRule.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 変更可能なルール
- */
-export interface ICustomizableRule {
-  get value(): any;
-  set value(newValue: any);
-}
