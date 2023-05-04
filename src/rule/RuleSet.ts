4e7b832e21e707f97812455608b9553031447128
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-04 15:43:46 +0900

    ルールのセットクラスを定義

---
+++ src/rule/RuleSet.ts
@@ -0,0 +1,40 @@
+import type { IRuleKeyValue } from "./RuleKeyValue";
+
+/**
+ * ルールのセット
+ */
+export class RuleSet {
+  private rules: {[key: string]: IRuleKeyValue<any>} = {};
+
+  /**
+   * ルールをセット
+   * @param rule ルール
+   */
+  setRule(rule: IRuleKeyValue<any>): void {
+    this.rules[rule.key] = rule;
+  }
+
+
+}
+
+/**
+ * ルールセットコンテキスト
+ * RuleSetクラスのreadonlyなデータ
+ */
+export class RuleSetContext {
+  /**
+   * コンストラクタ
+   * @param _rules ルールセットオブジェクト
+   */
+  constructor(private rules: {[key: string]: IRuleKeyValue<any>}) {
+  }
+
+  /**
+   * ルールの設定値を取得
+   * @param key キー
+   * @returns 設定値
+   */
+  getRuleValue<T>(key: string): T {
+    return this.rules[key].value() as T;
+  }
+}
