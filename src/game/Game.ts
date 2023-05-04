4eb54c7bfbeff9340804e659a487702add181b60
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-05 01:27:44 +0900

    メンバを移動して役割を明確にした

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,10 +1,16 @@
 import { GameSequenceBase } from "./GameSequence";
+import { Board } from "@src/board/Board";
+import { Hand } from "@src/hand/Hand";
+import { IThinkable } from "@src/player/Thinkable";
 
 /**
  * ゲームクラス
  */
 export class Game extends EventTarget {
   private sequence: GameSequenceBase | null = null;
+  private players: IThinkable[] = [];
+  private playerHands: Hand[] = [];
+  private board: Board = new Board();
 
   /**
    * ゲームシーケンスをセット
--- src/game/GameSequence.ts
+++ src/game/GameSequence.ts
@@ -1,6 +1,3 @@
-import { Board } from "@src/board/Board";
-import { Hand } from "@src/hand/Hand";
-import { IThinkable } from "@src/player/Thinkable";
 import { RuleBase } from "@src/rule/RuleBase";
 import { Tile } from "@src/tile/Tile";
 import { Game } from "./Game";
@@ -9,10 +6,6 @@ import { Game } from "./Game";
  * ゲームシーケンス基底クラス
  */
 export abstract class GameSequenceBase {
-  protected players: IThinkable[] = [];
-  protected playerHands: Hand[] = [];
-  protected board: Board = new Board();
-
   /**
    * コンストラクタ
    * @param ruleBase ルール基底クラス
