fce11caf24ef727cb00187cfc524a004fa2a48f1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-04 18:28:18 +0900

    「場風と局数」を表すValueObjectが抜けていたので追加

---
+++ src/board/Round.ts
@@ -0,0 +1,25 @@
+import { Wind } from "./Wind";
+
+/**
+ * 場風と局数
+ * 例：東一局
+ */
+export class Round {
+  /**
+   * コンストラクタ
+   * @param _wind 場風
+   * @param _roundNum 局数
+   */
+  constructor(private _wind: Wind, private _roundNum: number) {
+  }
+
+  /**
+   * 場風
+   */
+  get wind(): Wind { return this._wind; }
+
+  /**
+   * 局数
+   */
+  get roundNum(): number { return this._roundNum; }
+}
