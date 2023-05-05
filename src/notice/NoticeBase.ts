b1be808ce48a7ee920ef90eaf9c5cbffcc54c5f4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-05 11:31:15 +0900

    通知するイベントの基底クラス

---
+++ src/notice/NoticeBase.ts
@@ -0,0 +1,32 @@
+import { RuleBase } from "@src/rule/RuleBase";
+import { RuleSetContext } from "@src/rule/RuleSet";
+import { IYaku } from "@src/winning_hand/Yaku";
+
+/**
+ * 通知の基底クラス
+ */
+export class NoticeBase extends Event {  
+  private _ruleContext: RuleSetContext;
+  private _yakuList: IYaku[];
+  
+  /**
+   * コンストラクタ
+   * @param noticeType 通知タイプ
+   * @param rule ルールオブジェクト
+   */
+  constructor(noticeType: string, private rule: RuleBase) {
+    super(noticeType);
+    this._ruleContext = rule.getRuleSetContext();
+    this._yakuList = rule.yakuList;
+  }
+
+  /**
+   * ルール
+   */
+  get ruleContext(): RuleSetContext { return this._ruleContext; }
+
+  /**
+   * 採用役リスト
+   */
+  get yakuList(): IYaku[] { return this._yakuList.concat(); }
+}
